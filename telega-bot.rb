b61802fadd1b7d8ff5eca39d08748ce841f1edbe
Alexey Ovsov <ovsovalexey@gmail.com>
2016-08-10 22:24:43 +0300

    description

--- telega-bot.rb
+++ telega-bot.rb
@@ -6,7 +6,7 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.sendMessage(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      bot.api.sendMessage(chat_id: message.chat.id, text: "Hello darling, #{message.from.first_name}")
     end
   end
 end
